4cd934bbf0a6733b1204be86c64884aaeb962ee5
JDutra <dutraneto@me.com>
2020-07-24 16:28:14 -0400

    Config jest initial structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,57 +4,71 @@ type Props = {
     title: string
 }
 
+
 export default function Home({ title = 'Next tutorial' }: Props) {
     return (
-        <div className='container'>
+        <div className="container">
             <Head>
                 <title>{title}</title>
-                <link rel='icon' href='/favicon.ico' />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>
-                <h1 className='title'>{title}</h1>
+                <h1 className="title">{title}</h1>
 
-                <p className='description'>
+                <p className="description">
                     Get started by editing <code>pages/index.js</code>
                 </p>
 
-                <div className='grid'>
-                    <a href='https://nextjs.org/docs' className='card'>
+                <div className="grid">
+                    <a href="https://nextjs.org/docs" className="card">
                         <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
+                        <p>
+                            Find in-depth information about Next.js features and
+                            API.
+                        </p>
                     </a>
 
-                    <a href='https://nextjs.org/learn' className='card'>
+                    <a href="https://nextjs.org/learn" className="card">
                         <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
+                        <p>
+                            Learn about Next.js in an interactive course with
+                            quizzes!
+                        </p>
                     </a>
 
                     <a
-                        href='https://github.com/vercel/next.js/tree/master/examples'
-                        className='card'
+                        href="https://github.com/vercel/next.js/tree/master/examples"
+                        className="card"
                     >
                         <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
+                        <p>
+                            Discover and deploy boilerplate example Next.js
+                            projects.
+                        </p>
                     </a>
 
                     <a
-                        href='https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-                        className='card'
+                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        className="card"
                     >
                         <h3>Deploy &rarr;</h3>
-                        <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+                        <p>
+                            Instantly deploy your Next.js site to a public URL
+                            with Vercel.
+                        </p>
                     </a>
                 </div>
             </main>
 
             <footer>
                 <a
-                    href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-                    target='_blank'
-                    rel='noopener noreferrer'
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
                 >
-                    Powered by <img src='/vercel.svg' alt='Vercel Logo' className='logo' />
+                    Powered by{' '}
+                    <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
                 </a>
             </footer>
 
@@ -133,8 +147,9 @@ export default function Home({ title = 'Next tutorial' }: Props) {
                     border-radius: 5px;
                     padding: 0.75rem;
                     font-size: 1.1rem;
-                    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-                        Bitstream Vera Sans Mono, Courier New, monospace;
+                    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                        DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
+                        monospace;
                 }
 
                 .grid {
@@ -194,8 +209,9 @@ export default function Home({ title = 'Next tutorial' }: Props) {
                 body {
                     padding: 0;
                     margin: 0;
-                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-                        Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
+                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+                        Helvetica Neue, sans-serif;
                 }
 
                 * {
