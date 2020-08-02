7e47cb91d3649bd9438999a5cdeca29362eea2bb
JDutra <dutraneto@me.com>
2020-08-01 23:22:55 -0400

    Add styled components and config jest tests

--- src/pages/components/Main/test.tsx
+++ src/pages/components/Main/test.tsx
@@ -4,12 +4,17 @@ import Main from '.'
 
 describe('<Main />', () => {
     it('should render the heading', () => {
-        const {container} = render(<Main />)
+        const { container } = render(<Main />)
 
         expect(
             screen.getByRole('heading', { name: /react avançado/i })
         ).toBeInTheDocument()
 
         expect(container.firstChild).toMatchSnapshot
+    })
+
+    it('should render colors correctly', () => {
+        const { container } = render(<Main />)
+        expect(container.firstChild).toHaveStyle({ background: '#06092b' })
     })
 })
