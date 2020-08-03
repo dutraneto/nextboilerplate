fc7b45d29da2261cf14363ba9314c820a8f6a6be
JDutra <dutraneto@me.com>
2020-08-02 22:25:20 -0400

    Config storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    stories: ['../src/components/**/*.stories.js'],
+    addons: [
+        '@storybook/preset-typescript',
+        '@storybook/addon-actions',
+        '@storybook/addon-links'
+    ]
+}
